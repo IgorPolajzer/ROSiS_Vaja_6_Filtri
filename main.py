b857008f2fb1515796d0948b5661c67ba016b069
Igor Polajžer <polajzerigor@gmail.com>
2026-05-30 16:09:38 +0200

    Fixed plot labels

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from filter import *
 from util import *
 
 if __name__ == '__main__':
-    file_name = "a_high_pitch.mp3"
+    file_name = "o_high_pitch.mp3"
     fo = 41
 
     y, Fs = read_signal_from_mp3(fr"recordings/{file_name}")
@@ -14,7 +14,7 @@ if __name__ == '__main__':
     harmonics = find_harmonics(y_fft, Fs, prominence=(50, None), distance=150)
     print(harmonics)
 
-    y_fil = design_fir(y, harmonics, Fs, fo)
-    # y_fil = design_iir(y, harmonics, Fs, fo, filter_type=Filter.CHEBYSHEV_I)
+    #y_fil = design_fir(y, harmonics, Fs, fo)
+    y_fil = design_iir(y, harmonics, Fs, fo, filter_type=Filter.CHEBYSHEV_I)
     y_fil_fft = np.fft.fft(y_fil)
     plot_fft(y_fil_fft, Fs)
